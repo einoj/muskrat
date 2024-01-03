c721965e178ed593d59cf1045a35c91c4df716d9
Eino Juhani Oltedal <einoju@gmail.com>
2024-01-03 16:03:00 +0100

    Clean up code
    Don't specify types when setting up the PWM it looks messy
    
    Use camel case to stop cargo from complaining

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use embassy_stm32::timer::simple_pwm::{PwmPin, SimplePwm};
 use embassy_stm32::timer::Channel;
 use embassy_time::Timer;
 //use embassy_stm32::gpio::{Input, Pull};
-use embassy_stm32::gpio::{AnyPin, Input, Level, Output, Pin, Pull, Speed};
+use embassy_stm32::gpio::{Input, Pull};
 use embassy_stm32::exti::ExtiInput;
 use {defmt_rtt as _, panic_probe as _};
 
@@ -25,33 +25,31 @@ async fn main(_spawner: Spawner) {
     let mut button = ExtiInput::new(button, p.EXTI1);
 
     // Create and initialize a delay variable to manage delay loop
-    let mut del_var = 2000;
+    let _del_var = 2000;
 
-    let chA: PwmPin<'_, embassy_stm32::peripherals::TIM3, embassy_stm32::timer::simple_pwm::Ch1> = PwmPin::new_ch1(p.PE3, OutputType::PushPull);
-    //let chB: PwmPin<'_, embassy_stm32::peripherals::TIM3, embassy_stm32::timer::simple_pwm::Ch3> = PwmPin::new_ch3(p.PE5, OutputType::PushPull);
-    let chC: PwmPin<'_, embassy_stm32::peripherals::TIM4, embassy_stm32::timer::simple_pwm::Ch2> = PwmPin::new_ch2(p.PD13, OutputType::PushPull);
+    let ch_a = PwmPin::new_ch1(p.PE3, OutputType::PushPull);
+    let ch_b = PwmPin::new_ch2(p.PD13, OutputType::PushPull);
 
-    //let mut pwmA = SimplePwm::new(p.TIM3, Some(chA), None, Some(chB), None, khz(10), Default::default());
-    let mut pwmA = SimplePwm::new(p.TIM3, Some(chA), None, None, None, khz(10), Default::default());
-    let mut pwmB: SimplePwm<'_, embassy_stm32::peripherals::TIM4> = SimplePwm::new(p.TIM4, None, Some(chC), None, None, khz(10), Default::default());
+    let mut pwm_a = SimplePwm::new(p.TIM3, Some(ch_a), None, None, None, khz(10), Default::default());
+    let mut pwm_b = SimplePwm::new(p.TIM4, None, Some(ch_b), None, None, khz(10), Default::default());
 
-    let maxA = pwmA.get_max_duty();
-    let maxB = pwmB.get_max_duty();
+    let max_a = pwm_a.get_max_duty();
+    let max_b = pwm_b.get_max_duty();
 
-    pwmA.enable(Channel::Ch1);
-    pwmB.enable(Channel::Ch2);
+    pwm_a.enable(Channel::Ch1);
+    pwm_b.enable(Channel::Ch2);
 
     info!("PWM initialized");
-    info!("PWM max duty {}", maxA);
+    info!("PWM max duty {}", max_a);
 //    let button = Input::new(p.PC13, Pull::Down);
 
     loop {
         button.wait_for_rising_edge().await;
-        pwmA.set_duty(Channel::Ch1, maxA);
-        pwmB.set_duty(Channel::Ch2, maxB);
+        pwm_a.set_duty(Channel::Ch1, max_a);
+        pwm_b.set_duty(Channel::Ch2, max_b);
         Timer::after_millis(1500).await;
-        pwmA.set_duty(Channel::Ch1, 0);
-        pwmB.set_duty(Channel::Ch2, 0);
+        pwm_a.set_duty(Channel::Ch1, 0);
+        pwm_b.set_duty(Channel::Ch2, 0);
         Timer::after_millis(1000).await;
     }
 }
