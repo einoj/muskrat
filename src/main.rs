11a2e94b587958a767f51815475c0ebfabbf45e2
Eino Juhani Oltedal <einoju@gmail.com>
2024-01-08 17:12:13 +0100

    Setup emitter/detector diodes
    All of them work except L_detector2.

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,38 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
+use embassy_stm32::Config;
 use embassy_stm32::gpio::OutputType;
 use embassy_stm32::time::khz;
 use embassy_stm32::timer::simple_pwm::{PwmPin, SimplePwm};
 use embassy_stm32::timer::Channel;
-use embassy_time::Timer;
-//use embassy_stm32::gpio::{Input, Pull};
-use embassy_stm32::gpio::{Input, Pull};
+use embassy_time::{Delay, Timer};
+use embassy_stm32::gpio::{Input, Level, Output, Pull, Speed};
 use embassy_stm32::exti::ExtiInput;
+use embassy_stm32::adc::{Adc, SampleTime};
+use embassy_stm32::rcc::{AdcClockSource, ClockSrc, Pll, PllMul, PllPreDiv, PllRDiv, PllSource};
+
 use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
-    let p = embassy_stm32::init(Default::default());
+
+    let mut config = Config::default();
+    config.rcc.hsi = true;
+    config.rcc.mux = ClockSrc::PLL1_R;
+    config.rcc.adc_clock_source = AdcClockSource::SYS;
+    config.rcc.pll = Some(Pll {
+        // 64Mhz clock (16 / 1 * 8 / 2)
+        source: PllSource::HSI,
+        prediv: PllPreDiv::DIV1,
+        mul: PllMul::MUL8,
+        divp: None,
+        divq: None,
+        divr: Some(PllRDiv::DIV2),
+    });
+    let mut p = embassy_stm32::init(config);
     info!("Muskrat OS!");
+
 
     let button = Input::new(p.PD1, Pull::None);
     let mut button = ExtiInput::new(button, p.EXTI1);
@@ -42,6 +60,39 @@ async fn main(_spawner: Spawner) {
     info!("Right PWM max duty {}", max_r);
     info!("Left PWM max duty {}", max_l);
 
+    //ADC12_IN7
+    let mut mf_emitter_1 = Output::new(p.PD4, Level::High, Speed::High);
+    let mut mf_emitter_2 = Output::new(p.PD5, Level::High, Speed::High);
+    let mut lf_emitter = Output::new(p.PD7, Level::High, Speed::High);
+    let mut l_emitter = Output::new(p.PD6, Level::High, Speed::High);
+    let mut rf_emitter = Output::new(p.PD2, Level::High, Speed::High);
+    let mut r_emitter = Output::new(p.PD3, Level::High, Speed::High);
+    mf_emitter_1.set_high();
+    mf_emitter_2.set_high();
+    lf_emitter.set_high();
+    l_emitter.set_high();
+    rf_emitter.set_high();
+    r_emitter.set_high();
+    let mut diode_adc = Adc::new(p.ADC2, &mut Delay);
+    diode_adc.set_sample_time(SampleTime::Cycles24_5);
+    loop {
+        let mf_diode = diode_adc.read(&mut p.PC3);
+        let lf_diode = diode_adc.read(&mut p.PC0);
+        let l1_diode = diode_adc.read(&mut p.PC1);
+        let l2_diode = diode_adc.read(&mut p.PC2);
+        let rf_diode = diode_adc.read(&mut p.PA2);
+        let r1_diode = diode_adc.read(&mut p.PA1);
+        let r2_diode = diode_adc.read(&mut p.PA0);
+        info!("
+            mf diode: {}
+            lf diode: {} l1_diode: {} l2_diode: {}
+            rf_diode: {} r1_diode: {} r2_diode: {}",
+            mf_diode, lf_diode, l1_diode,
+            l2_diode, rf_diode, r1_diode, r2_diode);
+        Timer::after_millis(200).await;
+    }
+
+    /*
     loop {
         button.wait_for_rising_edge().await;
         pwm_r.set_duty(Channel::Ch1, 0);
@@ -57,4 +108,5 @@ async fn main(_spawner: Spawner) {
         pwm_r.set_duty(Channel::Ch1, 0);
         pwm_l.set_duty(Channel::Ch2, 0);
     }
+    */
 }
